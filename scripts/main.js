55d7e4e187cfd341438cd5526b3d6342990e5284
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-11-20 22:51:45 -0500

    Live version

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,13 @@ functionality.scrollSpy = () => {
 	$(".header__menu").stickynav();
 }
 
+functionality.scrollReveal = () => {
+	const sr = ScrollReveal({ reset: true });
+	sr.reveal('.featured__project', { scale: 0.80, duration: 1000 });
+
+	sr.reveal('.laptop', { duration: 1000, distance: '200px', origin: 'right'});
+} 
+
 functionality.stickyNav = () => {
 	$(window).scroll(function () {
 		var sticky = $('.header__nav'),
@@ -12,7 +19,7 @@ functionality.stickyNav = () => {
 		if (scroll >= 100) sticky.addClass('header__nav--bg');
 		else sticky.removeClass('header__nav--bg');
 	});
-} 
+}
 
 functionality.hamburger = () => {
 	$('.menu__burger').on('click', () => {
@@ -21,7 +28,7 @@ functionality.hamburger = () => {
 }
 
 functionality.openModal = () => {
-	$('.portfolio__item').on('click', function(){
+	$('.portfolio__item').on('click', function () {
 		const id = $(this).attr('id');
 		$(`.modal__${id}`).addClass('modalOpen');
 		$('.filter').removeClass('hidden');
@@ -30,13 +37,14 @@ functionality.openModal = () => {
 
 functionality.closeModal = () => {
 
-	$('.modal__closeButton').on('click', function(e){
+	$('.modal__closeButton').on('click', function (e) {
 		e.stopPropagation();
 		$('.portfolio__modal').removeClass("modalOpen");
 		$(".filter").addClass("hidden");
 	});
 }
 
+// closes modals on background click
 $(document).click(function (e) {
 	if (!$(e.target).closest(".portfolio__modal,.portfolio__item").length) {
 		$("body").find(".portfolio__modal").removeClass("modalOpen");
@@ -44,10 +52,10 @@ $(document).click(function (e) {
 	}
 });
 
-
-$(document).ready(function() {
+$(document).ready(function () {
 	functionality.stickyNav();
 	functionality.scrollSpy();
+	functionality.scrollReveal();
 	functionality.hamburger();
 	functionality.openModal();
 	functionality.closeModal();
